7e4865be1d22d1a9a9affc93cab25243099edf5e
santhisrikh <santhisri.kha@gmail.com>
2025-02-05 16:57:15 +0530

    M4S1D2

---
+++ Module4/Sprint1/chakra-ui-project/src/Demo.jsx
@@ -0,0 +1,17 @@
+import { Heading, Button, Box } from "@chakra-ui/react";
+import React from "react";
+
+const Demo = () => {
+	return (
+		<>
+			<Box>
+				<Heading>Hi thi sis Demo</Heading>
+				<Button variant="subtle" colorPalette={"red"}>
+					Click me
+				</Button>
+			</Box>
+		</>
+	);
+};
+
+export default Demo;
